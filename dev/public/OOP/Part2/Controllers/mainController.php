547f75df69d4e6c86881c53159c6e4fe73bcc158
plugmxlly <117053595+plugmxlly@users.noreply.github.com>
2023-10-24 23:26:50 +0700

    View

--- dev/public/OOP/Part2/Controllers/mainController.php
+++ dev/public/OOP/Part2/Controllers/mainController.php
@@ -2,20 +2,28 @@
 
 namespace Controllers;
 
+use View\View;
+
 class MainController
 {
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new View(__DIR__ . '/../Templates');
+    }
+
     public function main()
     {
-        echo 'Главная страница';
+        $articles = [
+            ['name' => 'Статья 1', 'text' => 'Текст статьи 1'],
+            ['name' => 'Статья 2', 'text' => 'Текст статьи 2'],
+        ];
+        $this->view->renderHtml('Main/main.php', ['articles' => $articles]);
     }
 
     public function sayHello(string $name)
     {
-        echo 'Привет, ' . $name;
-    }
-
-    public function sayBye(string $name)
-    {
-        echo 'Пока, ' . $name;
+        $this->view->renderHtml('Main/hello.php', ['name' => $name]);
     }
 }
